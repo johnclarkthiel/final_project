25292753dc80aec2a74b1b5b0e9703e3ae1a81b9
John <johnclarkthiel@Johns-MBP.home>
2016-03-26 12:18:37 -0400

    adding login form

--- public/js/userauth.js
+++ public/js/userauth.js
@@ -1,8 +1,18 @@
 var app = angular.module("user-auth", ["ngRoute"]);
-//using http for ajax requests to the server, $scope to access elements on the page, $rootScope to make objects globally accessible and location to change partials to render new page views
-app.controller('AuthController', ['$http', '$scope', '$location', '$rootScope', function($http, $scope, $location, $rootScope) {
+//using http for ajax requests to the server, $scope to access elements on the page, $rootScope to make objects globally accessible and location to change partials to render new page views, using route params to get params in url
+app.controller('AuthController', ['$http', '$scope', '$location', '$rootScope', '$routeParams', function($http, $scope, $location, $rootScope, $routeParams) {
 	$rootScope.user = null;
+	var controller = this;
+	this.password = null;
+	this.id = $routeParams.id
+	//don't need to reference controller in index.html when using scope
+	$scope.authUser = function() {
+		console.log('AUTH USER WORKED!');
+	};
 
+	$scope.login = function() {
+		console.log('log in working');
+	};
 
 
 }]); //END USER CONTROLLER
@@ -12,7 +22,7 @@ app.config(['$routeProvider', 'locationProvider', function($routeProvider,$locat
 	$locationProvider.html5Mode({ enabled : true });
 	$routeProvider
 	.when('/login', {
-		templateUrl: 'partials/loginform.html',
+		templateUrl: 'partials/login.html',
 		controller: 'AuthController',
 		controllerAs: 'authCtrl'
 	}).otherwise({
