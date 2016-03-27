21aecd7f34158718b7424d19157029ffb94e4e4c
John <johnclarkthiel@Johns-MBP.home>
2016-03-27 15:39:38 -0400

    adding article search

--- public/js/search.js
+++ public/js/search.js
@@ -1,24 +1,38 @@
 var app = angular.module('add-search', ['ngRoute']);
 
 app.controller('SearchController', ['$http', '$scope', '$location', '$rootScope', '$routeParams', function($http,$scope,$location,$rootScope, $routeParams){
+	//values for showing movie, book, article, etc. search forms
 	this.searchMovies = false;
 	this.searchBooks = false;
+	this.searchArticles = false;
+	//values for results from searches ... initially set to zero
 	this.results = null;
 	this.bookresults = null;
 
-	//show movie search
+	//show movie search, cancel previous results
 	this.showMovieSearch = function() {
 		this.searchMovies = !this.searchMovies
 		this.searchBooks = false;
+		this.searchArticles = false;
 		this.bookresults = null;
+		this.articleresults = null;
 	};
-	//show book search
+	//show book search, cancel previous results
 	this.showBookSearch = function() {
 		this.searchBooks = !this.searchBooks 
 		this.searchMovies = false;
+		this.searchArticles = false;
 		this.results = null;
+		this.articleresults = null;
 	};
-
+	//show book search
+	this.showArticleSearch = function() {
+		this.searchArticles = true;
+		this.searchBooks = false;
+		this.searchMovies = false;
+		this.results = null;
+		this.bookresults = null;
+	};
 
 
 	this.searchForMovies = function() {
@@ -48,6 +62,22 @@ app.controller('SearchController', ['$http', '$scope', '$location', '$rootScope'
 			controller.bookresults = response.data.results;
 			console.log("CONTROLLER RESULTS ", controller.bookresults);
 			controller.bookSearch = undefined;
+		}, function(err){
+			console.log(err)
+		});
+	};
+
+	this.searchForArticles = function() {
+		console.log("SEARCH ARTICLES WORKS!");
+		console.log(this.articleSearch);
+		var controller = this;
+		$http.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + this.articleSearch + '&api-key=05a267cb078d932cbb8605e7a2f17656:16:51925285').
+		then(function(response){
+			controller.showArticleResults = true;
+			console.log(response);
+			controller.articleresults = response.data.response.docs;
+			console.log("CONTROLLER RESULTS ", controller.articleresults);
+			controller.articleSearch = undefined;
 		}, function(err){
 			console.log(err)
 		});
