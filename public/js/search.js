6b03f3d4cc07789c61f023e6e67b775e9f3633d1
John <johnclarkthiel@Johns-MBP.home>
2016-03-27 14:43:35 -0400

    adding book search by author api

--- public/js/search.js
+++ public/js/search.js
@@ -3,17 +3,23 @@ var app = angular.module('add-search', ['ngRoute']);
 app.controller('SearchController', ['$http', '$scope', '$location', '$rootScope', '$routeParams', function($http,$scope,$location,$rootScope, $routeParams){
 	this.searchMovies = false;
 	this.searchBooks = false;
+	this.results = null;
+	this.bookresults = null;
 
 	//show movie search
 	this.showMovieSearch = function() {
 		this.searchMovies = !this.searchMovies
 		this.searchBooks = false;
+		this.bookresults = null;
 	};
 	//show book search
 	this.showBookSearch = function() {
 		this.searchBooks = !this.searchBooks 
 		this.searchMovies = false;
+		this.results = null;
 	};
+
+
 
 	this.searchForMovies = function() {
 		console.log("SEARCH MOVIES WORKS!");
@@ -25,6 +31,7 @@ app.controller('SearchController', ['$http', '$scope', '$location', '$rootScope'
 			console.log(response);
 			controller.results = response.data.results;
 			console.log("CONTROLLER RESULTS ", controller.results);
+			controller.movieSearch = undefined;
 		}, function(err){
 			console.log(err)
 		});
@@ -33,9 +40,14 @@ app.controller('SearchController', ['$http', '$scope', '$location', '$rootScope'
 	this.searchForBooks = function() {
 		console.log("SEARCH BOOKS WORKS!");
 		console.log(this.bookSearch);
-		$http.get('http://api.rottentomatoes.com/api/public/v1.0/movies/770672122/reviews.json?apikey=pcb64qevhv27w3atf5aenpkm').
+		var controller = this;
+		$http.get('http://api.nytimes.com/svc/books/v3/reviews/search.json?author=' + this.bookSearch + '&api-key=41d122abcaa833ff30fe9583273379d3:18:51925285').
 		then(function(response){
+			controller.showBookResults = true;
 			console.log(response);
+			controller.bookresults = response.data.results;
+			console.log("CONTROLLER RESULTS ", controller.bookresults);
+			controller.bookSearch = undefined;
 		}, function(err){
 			console.log(err)
 		});
