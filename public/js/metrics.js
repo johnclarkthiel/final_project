dcad8d1a3248d6a87c2945d1d1da2cb17cbc0ecb
John <johnclarkthiel@Johns-MBP.home>
2016-03-31 17:29:41 -0400

    adding more metrics

--- public/js/metrics.js
+++ public/js/metrics.js
@@ -2,8 +2,23 @@ var app = angular.module('metrics', ['ngRoute']);
 
 app.controller('MetricsController', ['$http', '$scope', '$location', '$rootScope', '$routeParams', function($http,$scope,$location,$rootScope, $routeParams){
 	this.ratings = $rootScope.user.ratings;
-	this.efficacy = $rootScope.user.efficacy;
+	this.efficacy = ($rootScope.user.efficacy / this.ratings.length) * 100;
 	this.searches = $rootScope.user.searches;
 	this.boredInstances = $rootScope.user.bored_instances;
+	this.nums = []
+	for (var i = 0; i < this.ratings.length; i ++) {
+		this.nums.push(parseInt(this.ratings[i]));
+	}
+	this.count = 0;
+	for (var i = 0; i < this.nums.length; i++) {
+		this.count += this.nums[i];
+	}
+
+	this.avg = (this.count / this.nums.length);
+	// this.total = this.nums.map(function(num, sum){
+	// 	console.log(num);
+	// 	console.log(sum);
+	// });
+
 
 }]);//end metrics controller
