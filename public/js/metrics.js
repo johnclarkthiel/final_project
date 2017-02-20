d3212b78b3bd1c463811081cca548b0ab69cdc50
John <johnclarkthiel@Johns-MacBook-Pro.local>
2017-02-20 14:02:40 -0500

    adding stuff

--- public/js/metrics.js
+++ public/js/metrics.js
@@ -1,6 +1,8 @@
 var app = angular.module('metrics', ['ngRoute']);
 
-app.controller('MetricsController', ['$http', '$scope', '$location', '$rootScope', '$routeParams', function($http,$scope,$location,$rootScope, $routeParams){
+app.controller('MetricsController', ['$http', '$scope', '$location', '$rootScope', '$routeParams', function($http,$scope,$location,	$rootScope, $routeParams){
+
+
 	// $http.get('/user/' + $rootScope.user._id).then(function(response){
 	// 	$rootScope.user = response.data;
 	// });
@@ -85,5 +87,27 @@ app.controller('MetricsController', ['$http', '$scope', '$location', '$rootScope
 
 	this.standardDeviation = Math.sqrt(this.variance);
 
+			//D3 example
+	var chartData = [4, 8, 15, 16, 23, 42];
+	d3.select(".chartExample")
+  .selectAll("div")
+    .data(chartData)
+  .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .text(function(d) { return d; });
+
+	var width = 420;
+	var barHeight = 20;
+
+	var x = d3.scaleLinear().domain([0, d3.max(chartData)]).range([0, width]);
+
+	var chart = d3.select(".chartExampleTwo").attr("width", width).attr("height", barHeight * chartData.length);
+
+	var bar = chart.selectAll("g").data(chartData).enter().append("g")
+	.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+	bar.append("rect").attr("width", x).attr("height", barHeight - 1);
+
+	bar.append("text").attr("x", function(d) { return x(d) - 3; }).attr("y", barHeight / 2).attr("dy", ".35em").text(function(d) { return d; });
 
 }]);//end metrics controller
